8271b80ea2e9b2a5f026b32386ff95362fa3c927
Justin Christensen <justin@promise.io>
2019-01-31 13:44:33 -0600

    remove stuff in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,8 @@
 module Main where
 
 import System.Environment.Config.Default
-import System.Environment (getArgs)
 
 main :: IO ()
 main = do
     config <- getConfig 
-    args <- getArgs
-    print args
     print (config :: FlatConfigMap)
